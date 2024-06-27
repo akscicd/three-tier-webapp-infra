4cab727a45a65b531fc92cee075ca339d88ecce3
Amit Kumar Shukla <110124443+akscicd@users.noreply.github.com>
2024-06-28 02:23:06 +0530

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -1,3 +1,6 @@
 output "db-host-ip" {
     value = google_sql_database_instance.db-instance.ip_address.0.ip_address
 }
+output "app_server_private_ip" {
+  value = google_compute_instance.app-server.network_interface[0].network_ip
+}
