70b74e7a3af0bac1eade76861f0dbdd401c7df77
Amit Kumar Shukla <110124443+akscicd@users.noreply.github.com>
2024-06-28 16:12:29 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,6 @@
 terraform {
+  backend "gcs" {
+  }
   required_providers {
     google = {
       source = "hashicorp/google"
@@ -6,6 +8,7 @@ terraform {
     }
   }
 }
+
 
 provider "google" {
   project = var.project_id
