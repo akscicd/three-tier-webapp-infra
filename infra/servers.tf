b0ad631b9bcf7845b858368e8606f1e0b1653ebe
Amit Kumar Shukla <110124443+akscicd@users.noreply.github.com>
2024-06-28 18:09:00 +0530

    Update servers.tf

--- infra/servers.tf
+++ infra/servers.tf
@@ -11,10 +11,16 @@ resource "google_compute_instance" "app-server" {
     network_interface {
       subnetwork = google_compute_subnetwork.subnet_private.self_link
     }
+
+    service_account {
+    email  = google_service_account.app_svc_acc.email
+    scopes = ["cloud-platform"]
+    }
     metadata_startup_script = <<-EOF
     #!/bin/bash
     sudo apt-get update
     sudo apt-get install docker.io -y
+    gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://europe-west1-docker.pkg.dev
     sudo docker pull europe-west1-docker.pkg.dev/dynamic-parity-426014-s3/akscicdprivate/backend:latest
     sudo docker run -itd -p 80:80 europe-west1-docker.pkg.dev/dynamic-parity-426014-s3/akscicdprivate/backend:latest
     EOF
@@ -36,11 +42,16 @@ resource "google_compute_instance" "web-server" {
         
       }
     }
+    service_account {
+    email  = google_service_account.app_svc_acc.email
+    scopes = ["cloud-platform"]
+    }
     tags = ["http-server"]
     metadata_startup_script = <<-EOF
     #!/bin/bash
     sudo apt-get update
     sudo apt-get install docker.io -y
+    gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://europe-west1-docker.pkg.dev
     sudo docker pull europe-west1-docker.pkg.dev/dynamic-parity-426014-s3/akscicdprivate/frontend:latest
     sudo docker run -itd -p 80:80 europe-west1-docker.pkg.dev/dynamic-parity-426014-s3/akscicdprivate/frontend:latest
     EOF
