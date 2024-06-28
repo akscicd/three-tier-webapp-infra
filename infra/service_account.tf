66b60e9668706f32d97bc81b2c83ee78ea3b8f14
Amit Kumar Shukla <110124443+akscicd@users.noreply.github.com>
2024-06-28 20:18:16 +0530

    Update service_account.tf

--- infra/service_account.tf
+++ infra/service_account.tf
@@ -11,11 +11,38 @@ resource "google_project_iam_binding" "artifactregistry_admin_binding" {
   ]
 }
 
-resource "google_project_iam_binding" "owner_binding" {
+resource "google_project_iam_binding" "editor_binding" {
   project = var.project_id
-  role    = "roles/owner"
+  role    = "roles/editor"
+
   members = [
     "serviceAccount:${google_service_account.app_svc_acc.email}",
   ]
 }
 
+resource "google_project_iam_binding" "iam_admin_binding" {
+  project = var.project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+
+  members = [
+    "serviceAccount:${google_service_account.app_svc_acc.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "compute_admin_binding" {
+  project = var.project_id
+  role    = "roles/compute.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.app_svc_acc.email}",
+  ]
+}
+
+resource "google_service_account_iam_binding" "user_sa_access" {
+  service_account_id = google_service_account.app_svc_acc.name
+  role               = "roles/iam.serviceAccountUser"
+
+  members = [
+    "user:amit.stp2001@gmail.com",
+  ]
+}
